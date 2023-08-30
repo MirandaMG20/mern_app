34afd0bb606bbf93527a9da4af1644ba1d9be12f
MirandaMG20 <guadamiran@gmail.com>
2023-08-30 16:05:46 -0400

    Full CRUD Backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
+// Not require
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
 
-const {errorHandler} = require('./middleware/errorMiddleware')
+const {errorHandler} = require('./middleware/errorMiddleware') // Not require
+const connectDB = require('./config/db')
 
-
+connectDB()
 // This 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -13,6 +16,6 @@ app.use(express.urlencoded({ extended: false }))
 // Middleware = use (goalRoutes to handle any endpoints that end with /api/goals)
 app.use('/api/goals', require('./routes/goalRoutes'))
 
-app.use(errorHandler)
+app.use(errorHandler) // Not require
 
 app.listen(port, () => { console.log(`Server started on port ${port}`)})
