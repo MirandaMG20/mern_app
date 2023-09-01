5737a2a0822a256f9ecc386ab8c20092bb52bdaf
MirandaMG20 <guadamiran@gmail.com>
2023-09-01 10:58:04 -0400

    full Crud

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -61,18 +61,24 @@ const updateGoals = asyncHandler(async (req, res) => {
 })
 
 //@desc     delete goals
-//@route    DELETE /api/goals
+//@route    DELETE /api/goals/:id
 //@access   Private
 const deleteGoals = asyncHandler(async (req, res) => {
-    
+    // Find the id
+    const goal = await Goal.deleteOne(req.params.id)
     // Check if goal exists
     if (!goal) {
         res.status(400)
         throw new Error('Goal not found')
     }
 
-    // Check for user
     const user = await User.findById(req.user.id)
+
+    // Check for user
+    if (!user) {
+        res.status(401)
+        throw new error('User not found')
+    }
 
     // Make sure the logged in user matches the goal user
     if (goal.user.toString() !== user.id) {
@@ -80,8 +86,7 @@ const deleteGoals = asyncHandler(async (req, res) => {
         throw new Error('User not authorized')
     }
 
-    // Find the id
-    const goal = await Goal.findByIdAndRemove(req.params.id)
+    await goal.deleteOne()
 
     res.status(200).json({ id: req.params.id })
 })
--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const goalSchema = mongoose.Schema({
     user: {
-        type: mongoose.Schema.Types.ObjectId, //id of the user
+        type: mongoose.Schema.Types.ObjectId, // Id of the user
         required: true,
-        ref: 'User' //reference to the User model
+        ref: 'User' // Reference to the User model
     },
     text: {
         type: String,
@@ -17,6 +17,8 @@ const goalSchema = mongoose.Schema({
 );
 
 module.exports = mongoose.model('Goal', goalSchema);
+
+
 
 // const mongoose = require('mongoose')
 
