15af5bbf3d6e3999b01e11225617b1e95ef85a91
MirandaMG20 <guadamiran@gmail.com>
2023-09-02 13:49:04 -0400

    GoalForm

---
+++ frontend/src/components/GoalForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
+import { UseSelector, useDispatch } from 'react-redux'
+
+function GoalForm() {
+    return (
+        <div>GoalForm</div>
+    )
+}
+
+export default GoalForm
